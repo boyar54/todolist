151a09a53e02191ad0a91ad05f6437e37a4bda15
boyar55 <bondy2526@gmail.com>
2022-10-07 21:43:21 +0300

    created basic visual tags

---
+++ src/ToDoList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ToDoList = () => {
+    return (
+        <div>
+            <div>
+                <p>Todo List</p>
+                <input/>
+                <button>
+                    Add
+                </button>
+            </div>
+
+        </div>
+    )
+}
+
+export default ToDoList
